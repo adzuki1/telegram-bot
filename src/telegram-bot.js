0832e314465d6ab03b71014209ff9864095864a4
adzuki1 <mneto.fis@gmail.com>
2025-10-19 18:25:45 -0300

    Program now use date

--- src/telegram-bot.js
+++ src/telegram-bot.js
@@ -1,13 +1,13 @@
 // global variables used in functions
-var token = "8240070489:AAGFhdCeNaaPZXvuej23uuQsxGJk1VWDi00";
+var token = "paste your token here";
 var telegram_url = "https://api.telegram.org/bot" + token;
-var web_app_url = "https://script.google.com/macros/s/AKfycbw0ZLJHlV0S3mCxyxxLMx--_PmHGohoH0z1D6KU5joe_HmYdU9FE9_yipPS55HV6A8o/exec";
+var web_app_url = "paste web app URL after deploy";
 
 //
 function setWebhook(){
   var url = telegram_url + "/setWebhook?url=" + web_app_url;
   var response = UrlFetchApp.fetch(url);
-  // Logger.log(response.getContentText()); // para verificar a resposta no log
+  // Logger.log(response.getContentText()); // if needed to verify log
 }
 
 //
@@ -23,14 +23,17 @@ function doPost(e){
   var text = contents.message.text;
   sendMessage(id, "Confirmed, received.");
 
-  var spreadsheet_id = "1plnBYQ9kShQGhfkVPKtHeMar0k1ThtvTahYIVLvx49E"; 
+  var spreadsheet_id = "id founded at URL"; 
   var sheet = SpreadsheetApp.openById(spreadsheet_id).getSheetByName("Sheet1");
   var dateNow = new Date;
 
-  sheet.appendRow([text, "outro texto", 1234]);
+  sheet.appendRow([text, "outro texto", 1234]); //
 }
 
+// storing simple date format: dd/mm/yyyy
 function dateNow(){
   var dateNow = new Date;
+   var reformmated_date = dateNow.getDate() + "/" + (dateNow.getMonth() + 1) + "/" + dateNow.getFullYear();
   Logger.log(dateNow);
+  Logger.log(reformmated_date);
 }
